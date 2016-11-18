10d7d5b3b163c42dbdc5ad0e9f0e10156dbac056
TreasureSchool <silversurfer@outlook.dk>
2016-11-18 09:12:47 +0100

    Uploading for Travis

--- src/test/java/dk/cphbusiness/cphbusiness/sem3/ci/MyFunctionalityTest.java
+++ src/test/java/dk/cphbusiness/cphbusiness/sem3/ci/MyFunctionalityTest.java
@@ -3,10 +3,6 @@ package dk.cphbusiness.cphbusiness.sem3.ci;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-/**
- *
- * @author jens
- */
 public class MyFunctionalityTest {
 
   @Test
